a59f44d6bf3fb582f9b3f8c27e099b807d5915bc
CharmedZ <fiestaadam10@gmail.com>
2025-11-22 15:47:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -402,7 +402,7 @@ socket.on("set_username", ({ username }) => {
     players.delete(id);
     socket.broadcast.emit("player_leave", { id });
   });
-
+});
 // --- Continuous orbit updates + combat ---
 setInterval(() => {
   const now = Date.now();
